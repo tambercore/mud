eafe5653c24fc12cdf458cc7c0b91f285a7dd0a9
Toby Clark <psytc9@nottingham.ac.uk>
2024-11-21 18:38:54 +0000

    add: fix code

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 mod ccg;
 mod brill;
 mod montague;
+mod wordnet;
 
 use std::io::{self, Write};
 
 fn main() -> io::Result<()> {
+    use wordnet::interface::get_meanings;
+
     let b = get_meanings("murder");
     println!("{:?}", b);
-    return;
+
+    return Ok(());
 }
