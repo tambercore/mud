7af90a5cbe3226d7576098e3b7a1e3f65175672a
Toby Clark <psytc9@nottingham.ac.uk>
2025-01-07 22:12:36 +0000

    doc: abstractions lambda'

--- src/lambda/abstraction.rs
+++ src/lambda/abstraction.rs
@@ -13,10 +13,14 @@ impl Substitutable for Abstraction {
         let bound_variable = &self.bound_var;
         let subexpr = &self.body;
 
+        // If we're substituting the bound variable ignore.
+        // TODO: We should fix this at some point, using De Bruijin Indicies, renaming the BV.
         if *&self.bound_var == Box::from(source.clone()) {
             *λAbs!(bound_variable.clone(), subexpr.clone())
+
+        // Otherwise, substitute in the sub expression.
         } else {
-            let subexpr_substituted = subexpr.substitute(source, target);
+            let subexpr_substituted: LambdaEntity = subexpr.substitute(source, target);
             *λAbs!(bound_variable.clone(), Box::new(subexpr_substituted))
         }
     }
