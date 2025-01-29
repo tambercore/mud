2cf82c21ef1fd94f4ea637c4b4cd7d935b75534a
Toby Clark <psytc9@nottingham.ac.uk>
2025-01-29 21:58:50 +0000

    add: conjunction remaster

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ fn main() {
 
     // TODO: Contractions break the tagger (don't does not get a tag etc)
 
-    let sentence = "every man walks";
+    let sentence = "man runs and eats cake";
 
     // retrieve words and their corresponding pos tags
     let vec_of_word_tag_tuples = tag_sentence(sentence, &lexical_ruleset, &contextual_ruleset, &mut wc_mapping);
