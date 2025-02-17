cfea407b31983feeaeb3191412131e94ec1939f9
Toby Clark <psytc9@nottingham.ac.uk>
2025-02-17 17:29:49 +0000

    add: derive agdaify for master type class

--- src/type_theory/master_type.rs
+++ src/type_theory/master_type.rs
@@ -1,7 +1,17 @@
 use crate::type_theory::function_type::FunctionType;
 use crate::type_theory::simple_type::SimpleType;
+use crate::type_theory::utility::Agdaify;
 
 pub enum Type {
     Simple(SimpleType),
     Func(FunctionType)
+}
+
+impl Agdaify for Type {
+    fn to_agda(&self) -> String {
+        match self {
+            Type::Simple(smp) => {smp.to_agda()}
+            Type::Func(fun) => {fun.to_agda()}
+        }
+    }
 }
