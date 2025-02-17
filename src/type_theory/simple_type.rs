536e0469fa2c2bcd0cfbb56b14b1e3d7b59d5f8c
Toby Clark <psytc9@nottingham.ac.uk>
2025-02-17 17:28:39 +0000

    add: Agdaify for simple types

--- src/type_theory/simple_type.rs
+++ src/type_theory/simple_type.rs
@@ -1,4 +1,12 @@
+use uuid::fmt::Simple;
+use crate::type_theory::utility::Agdaify;
 
 pub struct SimpleType {
     iden: String
+}
+
+impl Agdaify for SimpleType {
+    fn to_agda(&self) -> String {
+        format!("{}", self.iden)
+    }
 }
