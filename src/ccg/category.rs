09777aabf54d87f14c5173a65269c7a3a2b16905
aswarbs <amberswarbrick1@gmail.com>
2025-03-13 20:39:18 +0000

    docs: document `category.rs`

--- src/ccg/category.rs
+++ src/ccg/category.rs
@@ -2,8 +2,8 @@ use std::fmt;
 use serde::{Deserialize, Deserializer};
 use crate::ccg::type_parser::parse_category;
 
-
-
+/// Structure to represent types in the CCG, matching `lambeq`'s CCGType Structure.
+/// For more information, see: https://docs.quantinuum.com/lambeq/root-api.html#lambeq.CCGType.
 #[derive(Debug, Clone, PartialEq)]
 pub enum CCGType {
     ForwardsFunctor(Box<CCGType>, Box<CCGType>),
@@ -18,8 +18,6 @@ pub enum CCGType {
     Empty
 }
 
-
-
 /// Implementation for `Deserialize` to parse CCG categories from JSON strings.
 impl<'de> Deserialize<'de> for CCGType {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
@@ -31,26 +29,20 @@ impl<'de> Deserialize<'de> for CCGType {
     }
 }
 
-
-impl CCGType {
-    pub fn to_string(&self) -> String {
-        match self {
-            CCGType::ForwardsFunctor(left, right) => format!("({} / {})", left.to_string(), right.to_string()),
-            CCGType::BackwardsFunctor(left, right) => format!("({} \\ {})", left.to_string(), right.to_string()),
-            CCGType::Conjunction => "CONJ".to_string(),
-            CCGType::ConjunctionTag => "[CONJ]".to_string(),
-            CCGType::Noun => "N".to_string(),
-            CCGType::NounPhrase => "NP".to_string(),
-            CCGType::PrepositionalPhrase => "P".to_string(),
-            CCGType::Punctuation => "PUNC".to_string(),
-            CCGType::Sentence => "S".to_string(),
-            CCGType::Empty => "NONE".to_string(),
-        }
-    }
-}
-
+/// Implementation of pretty prints for `CCGType`.
 impl fmt::Display for CCGType {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.to_string())
+        match self {
+            CCGType::ForwardsFunctor(left, right) => write!(f, "({} / {})", left, right),
+            CCGType::BackwardsFunctor(left, right) => write!(f, "({} \\ {})", left, right),
+            CCGType::Conjunction => write!(f, "CONJ"),
+            CCGType::ConjunctionTag => write!(f, "[CONJ]"),
+            CCGType::Noun => write!(f, "N"),
+            CCGType::NounPhrase => write!(f, "NP"),
+            CCGType::PrepositionalPhrase => write!(f, "P"),
+            CCGType::Punctuation => write!(f, "PUNC"),
+            CCGType::Sentence => write!(f, "S"),
+            CCGType::Empty => write!(f, "NONE"),
+        }
     }
 }
