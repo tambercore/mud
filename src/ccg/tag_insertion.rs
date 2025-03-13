1f49ac4617d1dad35aa18a0169b053a06b4256db
aswarbs <amberswarbrick1@gmail.com>
2025-03-13 21:29:48 +0000

    docs: document `tag_insertion.rs`

--- src/ccg/tag_insertion.rs
+++ src/ccg/tag_insertion.rs
@@ -3,10 +3,12 @@ use crate::brill::wordclass::Wordclass;
 use crate::ccg::node::CCGNode;
 use crate::ccg::word::CCGWord;
 
-// Function that recursively add tags
+
+
+/// Function to recursively add tags to a 'CCGNode' and its children.
 fn _insert_tags<I>(old_node: &CCGNode, words_and_tags_iter: &mut I) -> CCGNode where I: Iterator<Item = (String, Wordclass)>,
 {
-    // Recur through children.
+    /* Recursively insert tags in the children of a `CCGNode`. */
     let new_children = old_node.children.as_ref().map(|child_vec| {
         child_vec
             .iter()
@@ -14,10 +16,10 @@ fn _insert_tags<I>(old_node: &CCGNode, words_and_tags_iter: &mut I) -> CCGNode w
             .collect()
     });
 
-    // If this node has a word, pull one (word, tag) from the iterator
+    /* If this node is terminal, pull one (word, tag) from the iterator */
     let new_word = match &old_node.word {
         Some(old_word) => {
-            // Grab the next (String, Wordclass) pair, if there isn't enough, something has gone wrong (panic!).
+            /* Grab the next (String, Wordclass) pair, if there isn't enough, something has gone wrong (panic!). */
             let (updated_text, updated_tag) = words_and_tags_iter
                 .next()
                 .expect("Ran out of Wordclass pairs while reconstructing CCG tree.");
@@ -35,12 +37,13 @@ fn _insert_tags<I>(old_node: &CCGNode, words_and_tags_iter: &mut I) -> CCGNode w
         rule: old_node.rule.clone(),
         word: new_word,
         children: new_children,
-        id: Uuid::new_v4(), // Generate a unique ID
+        id: Uuid::new_v4(),
     }
 }
 
 
-// Function to insert tags
+
+/// Function to insert tags to a given CCG Tree.
 pub fn insert_tags(old_tree: &CCGNode, words_and_tags: Vec<(String, Wordclass)>) -> CCGNode {
     let mut iter = words_and_tags.into_iter();
     _insert_tags(old_tree, &mut iter)
