c20b832ba3e4bf4da3384d5d874b87d628a17adb
Toby Clark <psytc9@nottingham.ac.uk>
2025-04-14 16:53:13 +0100

    add: stuff

--- src/composer/conclusions.rs
+++ src/composer/conclusions.rs
@@ -14,7 +14,13 @@ pub fn compose_conclusions(conclusions: Vec<(String, AgdaExpr)>, f: &mut Program
 
     let mut conclusion_records = vec![];
     let mut assumtion_index = 1;
-    for (conc_name, conc_type) in conclusions {
+
+    let prose_iden = format!("\\section{{Theorems}}");
+    let prose = literate!(prose_iden);
+
+    f.insert_definition(prose);
+
+    for (idx, (conc_name, conc_type)) in conclusions.iter().enumerate() {
 
         let iden = format!("thm{}", to_unicode_subscript(assumtion_index));
         let hypothesis = function_type!(term!("KnowledgeBaseᵣ"), term!(conc_name));
@@ -24,7 +30,7 @@ pub fn compose_conclusions(conclusions: Vec<(String, AgdaExpr)>, f: &mut Program
         conclusion_records.push(func.clone());
         assumtion_index = assumtion_index + 1;
 
-        let prose_iden = format!("{}_lp", iden);
+        let prose_iden = format!("\\subsection{{Theorem {}: `{}'}}\n\n{}_lp", idx + 1, conc_name, iden);
         let prose = literate!(prose_iden);
 
         f.insert_definition(prose);
